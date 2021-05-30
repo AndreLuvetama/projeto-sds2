a612283745eca3d3439dd52d47a53e5ba44930c7
AndreLuvetama <mbiyavanga.luvetama@oi.net.br>
2021-05-29 22:15:04 -0300

    Buscas agrupadas (GROUP BY)

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SalesSumDTO.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dsvendas.dto;
+
+import com.devsuperior.dsvendas.entity.Seller;
+
+public class SalesSumDTO {
+	private String sellerName;
+	private Double sum;
+	
+	public SalesSumDTO() {}
+
+	public SalesSumDTO(Seller seller, Double sum) {
+		this.sellerName = seller.getName();
+		this.sum = sum;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public Double getSum() {
+		return sum;
+	}
+
+	public void setSum(Double sum) {
+		this.sum = sum;
+	}
+	
+	
+
+}
---
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -0,0 +1,46 @@
+package com.devsuperior.dsvendas.service;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SalesSuccessDTO;
+import com.devsuperior.dsvendas.dto.SalesSumDTO;
+import com.devsuperior.dsvendas.entity.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageble){
+		sellerRepository.findAll();// evita a repedtição de busca no BD
+		Page<Sale> result = repository.findAll(pageble);
+		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SalesSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SalesSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
+	}
+	
+	
+}
